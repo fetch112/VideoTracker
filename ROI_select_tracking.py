bf16f4c09a6ececffef53c6c4f4f666b305153f7
kimruru <aszqw65@naver.com>
2020-10-16 16:31:29 +0900

    Add files via upload

--- ROI_select_tracking.py
+++ ROI_select_tracking.py
@@ -1,36 +1,214 @@
 import cv2
 import numpy as np
 
-video_path ='D:/개인용폴더/데스크탑/비디오영상/Primitive Technology- Round hut.mp4'
-cap=cv2.VideoCapture(video_path)
-if not cap.isOpened():
-    exit()
-tracker = cv2.TrackerCSRT_create()
-ret, img = cap.read() #첫번째 프레임 읽기
-cv2.namedWindow("select window")
-cv2.imshow("select window",img)
-
-##setting ROI 내가 선택한 영역 저장
-rect= cv2.selectROI("select window",img, fromCenter=False, showCrosshair=True)
-cv2.destroyWindow("select window")
-# initialize  tracker
-tracker.init(img,rect)
-## 실행##
+isDragging = False                      # 마우스 드래그 상태 저장 
+x0, y0, w, h = -1,-1,-1,-1              # 영역 선택 좌표 저장
+blue, red = (255,0,0),(0,0,255)         # 색상 값 
+roi = (0,0,0,0)
 
 
-while True:
+def onMouse(event,x,y,flags,param):     # 마우스 이벤트 핸들 함수  ---①
+    global isDragging, x0, y0, frame, roi      # 전역변수 참조
+    if event == cv2.EVENT_LBUTTONDOWN:  # 왼쪽 마우스 버튼 다운, 드래그 시작 ---②
+        isDragging = True
+        x0 = x
+        y0 = y
+    elif event == cv2.EVENT_MOUSEMOVE:  # 마우스 움직임 ---③
+        if isDragging:                  # 드래그 진행 중
+            img_draw = frame.copy()       # 사각형 그림 표현을 위한 이미지 복제
+            cv2.rectangle(img_draw, (x0, y0), (x, y), blue, 2) # 드래그 진행 영역 표시
+            cv2.imshow('image', img_draw) # 사각형 표시된 그림 화면 출력
+    elif event == cv2.EVENT_LBUTTONUP:  # 왼쪽 마우스 버튼 업 ---④
+        if isDragging:                  # 드래그 중지
+            isDragging = False          
+            w = x - x0                  # 드래그 영역 폭 계산
+            h = y - y0                  # 드래그 영역 높이 계산
+            print("x:%d, y:%d, w:%d, h:%d" % (x0, y0, w, h))
+            if w > 0 and h > 0:         # 폭과 높이가 음수이면 드래그 방향이 옳음 ---⑤
+                img_draw = frame.copy()   # 선택 영역에 사각형 그림을 표시할 이미지 복제
+                cv2.rectangle(img_draw, (x0, y0), (x, y), red, 2) # 선택 영역에 빨간 사각형 표시      
+                #roi = frame[y0:y0+h, x0:x0+w] # 원본 이미지에서 선택 영영만 ROI로 지정 ---⑥
+                roi = (x0,y0,w,h)
 
-    ret, img= cap.read() #1프레임씩 읽기
+
+
+# 트랙커 객체 생성자 함수 리스트 ---①
+trackers = [cv2.TrackerBoosting_create,
+            cv2.TrackerMIL_create,
+            cv2.TrackerKCF_create,
+            cv2.TrackerTLD_create,
+            cv2.TrackerMedianFlow_create,
+            cv2.TrackerGOTURN_create, #버그로 오류 발생
+            cv2.TrackerCSRT_create,
+            cv2.TrackerMOSSE_create]
+trackerIdx = 0  # 트랙커 생성자 함수 선택 인덱스
+tracker = None
+isFirst = True
+
+video_src = 0 # 비디오 파일과 카메라 선택 ---②
+#video_src = "../img/highway.mp4"
+cap = cv2.VideoCapture(video_src)
+fps = cap.get(cv2.CAP_PROP_FPS) # 프레임 수 구하기
+delay = int(1000/fps)
+win_name = 'Tracking APIs'
+cv2.namedWindow('image')
+
+
+cv2.setMouseCallback('image', onMouse)
+
+while cap.isOpened():
+    ret, frame = cap.read()
     if not ret:
-        exit()
-    tracker.init(img,rect)
-    success, box = tracker.update(img)
-    left, top, w, h =[int(v) for v in box]
-    cv2.rectangle(img, pt1=(left,top), pt2=(left+w, top + h), color=(255,255,0),
-                    thickness=3)
-    cv2.imshow("img",img)
-    if cv2.waitKey(1)==ord('q'):
+        print('Cannot read video file')
         break
+    img_draw = frame.copy()
+    if tracker is None: # 트랙커 생성 안된 경우
+        cv2.putText(img_draw, "Press the Space to set ROI!!", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2,cv2.LINE_AA)
+    else:
+        ok, bbox = tracker.update(frame)   # 새로운 프레임에서 추적 위치 찾기 ---③
+        (x,y,w,h) = bbox
+        if ok: # 추적 성공
+            cv2.rectangle(img_draw, (int(x), int(y)), (int(x + w), int(y + h)), (0,255,0), 2, 1)
+        else : # 추적 실패
+            cv2.putText(img_draw, "Tracking fail.", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2,cv2.LINE_AA)
+    trackerName = tracker.__class__.__name__
+    cv2.putText(img_draw, str(trackerIdx) + ":"+trackerName , (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,255,0),2,cv2.LINE_AA)
 
+    cv2.imshow("image", img_draw)
+    key = cv2.waitKey(delay) & 0xff
     
+    if  roi[2] and roi[3]:         # 위치 설정 값 있는 경우
+        tracker = trackers[trackerIdx]()    #트랙커 객체 생성 ---⑤
+        isInit = tracker.init(frame, roi)
+    elif key in range(48, 56): # 0~7 숫자 입력   ---⑥
+        trackerIdx = key-48     # 선택한 숫자로 트랙커 인덱스 수정
+        if bbox is not None:
+            tracker = trackers[trackerIdx]() # 선택한 숫자의 트랙커 객체 생성 ---⑦
+            isInit = tracker.init(frame, bbox) # 이전 추적 위치로 추적 위치 초기화
+    elif key == 27 : 
+        break
+else:
+    print( "Could not open video")
+cap.release()
+cv2.destroyAllWindows()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+import cv2
+
+# 트랙커 객체 생성자 함수 리스트 ---①
+trackers = [cv2.TrackerBoosting_create,
+            cv2.TrackerMIL_create,
+            cv2.TrackerKCF_create,
+            cv2.TrackerTLD_create,
+            cv2.TrackerMedianFlow_create,
+            cv2.TrackerGOTURN_create, #버그로 오류 발생
+            cv2.TrackerCSRT_create,
+            cv2.TrackerMOSSE_create]
+trackerIdx = 0  # 트랙커 생성자 함수 선택 인덱스
+tracker = None
+isFirst = True
+
+video_src = 0 # 비디오 파일과 카메라 선택 ---②
+#video_src = "../img/highway.mp4"
+cap = cv2.VideoCapture(video_src)
+fps = cap.get(cv2.CAP_PROP_FPS) # 프레임 수 구하기
+delay = int(1000/fps)
+win_name = 'Tracking APIs'
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print('Cannot read video file')
+        break
+    img_draw = frame.copy()
+    if tracker is None: # 트랙커 생성 안된 경우
+        cv2.putText(img_draw, "Press the Space to set ROI!!", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2,cv2.LINE_AA)
+    else:
+        ok, bbox = tracker.update(frame)   # 새로운 프레임에서 추적 위치 찾기 ---③
+        (x,y,w,h) = bbox
+        if ok: # 추적 성공
+            cv2.rectangle(img_draw, (int(x), int(y)), (int(x + w), int(y + h)), (0,255,0), 2, 1)
+        else : # 추적 실패
+            cv2.putText(img_draw, "Tracking fail.", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2,cv2.LINE_AA)
+    trackerName = tracker.__class__.__name__
+    cv2.putText(img_draw, str(trackerIdx) + ":"+trackerName , (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,255,0),2,cv2.LINE_AA)
+
+    cv2.imshow(win_name, img_draw)
+    key = cv2.waitKey(delay) & 0xff
+    if key == ord(' ') or (video_src != 0 and isFirst): # 스페이스 바 또는 비디오 파일 최초 실행 ---④
+        isFirst = False
+        roi = cv2.selectROI(win_name, frame, False)
+        print(roi)  # 초기 객체 위치 설정
+        if roi[2] and roi[3]:         # 위치 설정 값 있는 경우
+            tracker = trackers[trackerIdx]()    #트랙커 객체 생성 ---⑤
+            isInit = tracker.init(frame, roi)
+    elif key in range(48, 56): # 0~7 숫자 입력   ---⑥
+        trackerIdx = key-48     # 선택한 숫자로 트랙커 인덱스 수정
+        if bbox is not None:
+            tracker = trackers[trackerIdx]() # 선택한 숫자의 트랙커 객체 생성 ---⑦
+            isInit = tracker.init(frame, bbox) # 이전 추적 위치로 추적 위치 초기화
+    elif key == 27 : 
+        break
+else:
+    print( "Could not open video")
+cap.release()
+cv2.destroyAllWindows()
+
+import cv2
+import numpy as np
+
+isDragging = False                      # 마우스 드래그 상태 저장 
+x0, y0, w, h = -1,-1,-1,-1              # 영역 선택 좌표 저장
+blue, red = (255,0,0),(0,0,255)         # 색상 값 
+
+def onMouse(event,x,y,flags,param):     # 마우스 이벤트 핸들 함수  ---①
+    global isDragging, x0, y0, img      # 전역변수 참조
+    if event == cv2.EVENT_LBUTTONDOWN:  # 왼쪽 마우스 버튼 다운, 드래그 시작 ---②
+        isDragging = True
+        x0 = x
+        y0 = y
+    elif event == cv2.EVENT_MOUSEMOVE:  # 마우스 움직임 ---③
+        if isDragging:                  # 드래그 진행 중
+            img_draw = img.copy()       # 사각형 그림 표현을 위한 이미지 복제
+            cv2.rectangle(img_draw, (x0, y0), (x, y), blue, 2) # 드래그 진행 영역 표시
+            cv2.imshow('img', img_draw) # 사각형 표시된 그림 화면 출력
+    elif event == cv2.EVENT_LBUTTONUP:  # 왼쪽 마우스 버튼 업 ---④
+        if isDragging:                  # 드래그 중지
+            isDragging = False          
+            w = x - x0                  # 드래그 영역 폭 계산
+            h = y - y0                  # 드래그 영역 높이 계산
+            print("x:%d, y:%d, w:%d, h:%d" % (x0, y0, w, h))
+            if w > 0 and h > 0:         # 폭과 높이가 음수이면 드래그 방향이 옳음 ---⑤
+                img_draw = img.copy()   # 선택 영역에 사각형 그림을 표시할 이미지 복제
+                cv2.rectangle(img_draw, (x0, y0), (x, y), red, 2) # 선택 영역에 빨간 사각형 표시
+                cv2.imshow('img', img_draw) # 빨간 사각형 그려진 이미지 화면 출력
+                roi = img[y0:y0+h, x0:x0+w] # 원본 이미지에서 선택 영영만 ROI로 지정 ---⑥
+                cv2.imshow('cropped', roi)  # ROI 지정 영역을 새창으로 표시
+                cv2.moveWindow('cropped', 0, 0) # 새창을 화면 좌측 상단에 이동
+                cv2.imwrite('./cropped.jpg', roi)   # ROI 영역만 파일로 저장 ---⑦
+                print("croped.")
+            else:
+                cv2.imshow('img', img)  # 드래그 방향이 잘못된 경우 사각형 그림 없는 원본 이미지 출력
+                print("좌측 상단에서 우측 하단으로 영역을 드래그 하세요.")
+
+img = cv2.imread('../img/sunset.jpg')
+cv2.imshow('img', img)
+cv2.setMouseCallback('img', onMouse) # 마우스 이벤트 등록 ---⑧
+cv2.waitKey()
+cv2.destroyAllWindows()
+"""
